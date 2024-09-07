950b8a4cb2a7f0b71880697e9672827e9e127ca3
Abdul Vahab A A <abdulvahabaa916@gmail.com>
2024-09-07 13:40:06 +0530

    feat:sidebar add

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { useState } from "react"
-import Header from "./components/Header/Header"
-
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 export const App = () => {
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
+  const [isSidebarOpen, setisSidebarOpen] = useState(false);
 
   const toggleDarkMode = () => {
-    setDarkMode(!darkMode)
-  }
-  return (
-    <div className={`${darkMode && 'dark' } font-quicksand`}>
-      <Header  toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
-  </div>
-  )
-}
+    setDarkMode(!darkMode);
+  };
 
-export default App
+  const toggleSidebar = () => {
+    setisSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className={`${darkMode && "dark"} font-quicksand`}>
+      <Header
+        toggleDarkMode={toggleDarkMode}
+        darkMode={darkMode}
+        toggleSidebar={toggleSidebar}
+      />
+      <Sidebar isSidebarOpen={isSidebarOpen} />
+    </div>
+  );
+};
+
+export default App;

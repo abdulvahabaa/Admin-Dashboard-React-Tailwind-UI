1a4b201cc3c28f2f488c6fa643327cf237458717
Abdul Vahab A A <abdulvahabaa916@gmail.com>
2024-09-07 18:56:15 +0530

    reafactor:profile section add

--- src/components/Profile/profile.jsx
+++ src/components/Profile/profile.jsx
@@ -1,7 +1,13 @@
+import DonutChart from "./DonutChart";
+import ShortCuts from "./ShortCuts";
+import User from "./User";
+
 function profile() {
   return (
     <div className="px-2 py-4  mt-2 bg-gray-200 rounded-lg w-full dark:bg-gray-700 lg:w-60 xl:w-80 flex flex-col justify-between gap-4">
-      profile
+      <User />
+      <ShortCuts />
+      <DonutChart/>
     </div>
   );
 }
